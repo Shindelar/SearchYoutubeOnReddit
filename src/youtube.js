358363fb658a545e893720db3839d99b6e38b496
Shindelar <invisiblue3@gmail.com>
2016-04-11 23:57:40 +0200

    small fix
    Now listens for diffrent events and loads before youtube's window.onload

--- src/youtube.js
+++ src/youtube.js
@@ -1,18 +1,10 @@
 var parent, container, btn, content;
 
-onReady = function(callback) {
-	if (document.readyState === "complete")
-		window.setTimeout(callback, 0);
-	else
-		window.addEventListener("load", callback, false);
-};
+window.addEventListener('load', function() {
+	addBtn() }, false);
 
-onReady(function() {
-	addBtn();
-	document.getElementsByTagName('video')[0].onloadstart = function() {
-		addBtn();
-	}
-});
+document.addEventListener('spfdone', function() {
+	addBtn() }, false);
 
 function addBtn() {
 	if(document.getElementById('watch8-action-buttons')) {
