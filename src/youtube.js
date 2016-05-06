b7a3ef5fdc7867ca7315cbe193c241a3e94186d0
Shindelar <invisiblue3@gmail.com>
2016-05-06 13:44:06 +0200

    modular code
    Fixing small typos in code

--- src/youtube.js
+++ src/youtube.js
@@ -5,8 +5,7 @@ var container = {
 	btn = {
 		"class": "yt-uix-button yt-uix-button-default yt-uix-tooltip reddit-default-btn" },
 	content = {
-		"class": "yt-uix-button-content"
-	}
+		"class": "yt-uix-button-content" };
 
 window.addEventListener('load', function() {
 	addBtn() }, false);
@@ -15,7 +14,7 @@ document.addEventListener('spfdone', function() {
 	addBtn() }, false);
 
 // appends button to youtube UI
-function addBtn(callback) {
+function addBtn() {
 	var parent = document.getElementById('watch8-secondary-actions');
 	if(parent) {
 		parent.appendChild(create('div', container))
