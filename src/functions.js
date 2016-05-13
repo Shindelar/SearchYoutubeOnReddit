b1091396a96e598d02079b42dd111f1396dd578b
Shindelar <invisiblue3@gmail.com>
2016-05-14 00:12:15 +0200

    refactoring code

--- src/functions.js
+++ src/functions.js
@@ -1,8 +1,47 @@
-function create(tagName, attributes, content) {
+// return DOM element given the parameters
+var create = function (tagName, attributes, text) {
 	var element = document.createElement(tagName);
 	for(var key in attributes) {
 		element.setAttribute(key, attributes[key]);
 	}
-	if(content) element.innerHTML = content;
+	if(text) element.innerHTML = text;
 	return element;
+}
+// append children given an array
+Element.prototype.appendChildren = function(a) { a.forEach(e => {this.appendChild(e)}) };
+// send xmlhttp request
+var sendRequest = function (url, callback) {
+	xhttp = new XMLHttpRequest();
+	// handles state change
+	xhttp.onreadystatechange = function() {
+		if(xhttp.readyState == 4) callback(shorten(xhttp.response)); //handleResponse(response.data.children);
+	};
+	xhttp.responseType = "json";
+	xhttp.timeout = 4000;
+	xhttp.open('GET', url, true);
+	xhttp.send();
+}
+// shorten response, return subreddits
+var shorten = function(response) {
+	switch(typeof response) {
+		case "object":
+			return response.data.children.map(e => ({
+				created_utc: e.data.created_utc,
+				permalink: e.data.permalink,
+				subreddit: e.data.subreddit,
+				upvotes: e.data.score
+			})).filter((e,i,a) => duplicate(e,i,a,"subreddit"));
+		default:
+			return null;
+	}
+	function duplicate(element, index, array, key) {
+		if(index) {
+			for(let i = 0; i < array.length; i++) {
+				if(array[i][key] === element[key] && i < index) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 }
--- src/youtube.js
+++ src/youtube.js
@@ -15,12 +15,51 @@ document.addEventListener('spfdone', function() {
 
 // appends button to youtube UI
 function addBtn() {
-	var parent = document.getElementById('watch8-secondary-actions');
+	var parent = document.getElementById('watch8-secondary-actions'),
+		url = window.location.href;
 	if(parent) {
+		url = "https://www.reddit.com/search.json?q=url%3A" + url.split("v=")[1].replace(/^-*/, "") + "&sort=top&t=all&limit=3";
 		parent.appendChild(create('div', container))
 			.appendChild(create('button', btn))
 			.appendChild(create('span', content, 'Reddit Thread'));
 
-		search();
+		sendRequest(url, handleResponse);
+	}
+}
+function handleResponse(response) {
+	var container = document.getElementById('redditSearch'),
+		btn = container.firstChild;
+	switch(response.length) {
+		case 3:
+		case 2:
+			btn.className += " yt-uix-menu-trigger";
+			btn.appendChild(create('span', {"class": "yt-uix-button-arrow yt-sprite" }));
+			container.appendChild(create('div',
+					{'class': 'yt-uix-menu-content yt-ui-menu-content yt-uix-kdb-nav yt-uix-menu-hidden yt-uix-kbd-nav yt-uix-menu-content-hidden'}))
+				.appendChild(create('ul'))
+				.appendChildren(
+					response.map(e => {
+						var element =  create('li');
+						element.appendChild(create('button', {'class': 'yt-ui-menu-item yt-ui-menu-close-on-select',
+								'onclick': permalink(e.permalink)}))
+							.appendChild(create('span', {'class': 'yt-ui-menu-item-label'},
+								'r/' + e.subreddit + ', ' + e.upvotes));
+						return element;
+					})
+				);
+			break;
+		case 1:
+			btn.setAttribute('onclick', permalink(response[0].permalink));
+			btn.setAttribute('title', 'r/' + response[0].subreddit);
+			btn.firstChild.innerHTML = 'r/' + response[0].subreddit;
+			break;
+		default:
+			let title = document.getElementById('eow-title').innerHTML;
+			btn.setAttribute('onclick', permalink('/r/videos/submit?title=' + title, + '&url=' + window.location.href));
+			btn.className += " yt-uix-button-has-icon no-icon-markup action-panel-trigger-share";
+			btn.setAttribute('title', 'Share on Reddit');
+	}
+	function permalink(v) {
+		return "window.open('https://reddit.com" + v + "')";
 	}
 }
